6720172a2f395a1fcd29fb1a61474cd8bc468606
ManuelCoral1998 <almacolo23@hotmail.com>
2021-03-20 10:40:26 -0500

    change in front.sh

--- front.sh
+++ front.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -aG docker ec2-user
+# sudo yum update -y
+# sudo amazon-linux-extras install docker
+# sudo service docker start
+# sudo usermod -aG docker ec2-user
 
-sudo docker pull <YOUR DOCKER HUB/IMAGE>
-sudo docker run --name front -p 8080:8080 -e BACKEND_HOST=${back_host} -d IMAGE
+# sudo docker pull <YOUR DOCKER HUB/IMAGE>
+# sudo docker run --name front -p 8080:8080 -e BACKEND_HOST=${back_host} -d IMAGE
+
+apt update
+apt install software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install -y ansible
+
+git clone https://github.com/ManuelCoral1998/Ansible-Class.git
+
+cd Ansible-Class/
+
+ansible-playbook -i ./inventory/hosts -l ui site.yml -vvv -e back_host=${back_host}
