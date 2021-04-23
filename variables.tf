12e91e2b9f23881926cf586293fbdb26e5d37064
Manuel Coral <almacolo23@hotmail.com>
2021-04-23 04:28:42 +0000

    adding chmod

--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable "aws_region" {
   type    = string
-  default = # THE REGION 
+  default = "us-east-2" 
 }
 
 variable "automation_vpc_id" {
-  type = string
-  default = # VPC ID
+  type    = string
+  default = "vpc-025ce59ecf7aed804"
 }
 
 variable "public_subnet_id_2c" {
-  type = string
-  default = # SUBNET ID
+  type    = string
+  default = "subnet-0c4d638d6183bde99"
 }
 
 variable "public_subnet_id_2a" {
-  type = string
-  default = # SUBNET ID
+  type    = string
+  default = "subnet-02018063ae6cb01aa"
 }
 
 variable "ami_id" {
   type    = string
-  default = "ami-09246ddb00c7c4fef"
+  default = "ami-08962a4068733a2b6"
 }
 
 variable "key_name" {
   type    = string
-  default = # YOUR KEY NAME
+  default = "rootAutomatizacion"
 }
 
 variable "tag_responsible" {
   type    = string
-  default = "<YOUR TAG>"
+  default = "rootAutomatizacion"
 }
